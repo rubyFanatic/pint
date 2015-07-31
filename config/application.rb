b5381bbed8fea67e134ebe85372d3396b8edf653
rubyfanatic <ram.manusani@gmail.com>
2015-07-30 22:04:08 -0700

    First commit

---
+++ config/application.rb
@@ -0,0 +1,15 @@
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Pinteresting
+  class Application < Rails::Application
+
+    config.active_record.raise_in_transactional_callbacks = true
+    config.assets.initialize_on_precompile = false
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+
+  resources :pins
+
+  devise_for :users
+  root "pages#home"
+
+  get "about" => "pages#about"
+
+end
